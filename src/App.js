30ede516b4cdd8bde680e5ee01fcaa9d340eaf88
oğulcan erişti <ogulcaneristi123@gmail.com>
2022-06-28 02:30:46 +0300

    configure router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Home, Cart, ItemDetails, NotFound } from 'pages';
+import Header from 'components/Header';
+import 'styles/main.scss';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/item/:id" component={ItemDetails} />
+        <Route path="/cart" component={Cart} />
+        <Route path="*" component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
