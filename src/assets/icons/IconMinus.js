899bc64657572ed9d4c71e02ff311df1ef76531b
oğulcan erişti <ogulcaneristi123@gmail.com>
2022-07-02 23:49:18 +0300

    add icons for increase and decrease item quantity

---
+++ src/assets/icons/IconMinus.js
@@ -0,0 +1,27 @@
+import * as React from 'react';
+
+const IconMinus = props => (
+  <svg
+    width={props?.width || 34}
+    height={props?.height || 34}
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+    viewBox="0 0 34 34"
+  >
+    <path
+      d="M33.5 17c0 9.113-7.387 16.5-16.5 16.5S.5 26.113.5 17 7.887.5 17 .5 33.5 7.887 33.5 17Z"
+      stroke="#DDD"
+    />
+    <path
+      d="M23 17H11"
+      stroke="#0071DC"
+      strokeWidth={2}
+      strokeMiterlimit={10}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export default IconMinus;
