87e1fd90dec1e36236047aafdff98f9efc77e4b7
oğulcan erişti <ogulcaneristi123@gmail.com>
2022-07-03 03:55:49 +0300

    add get single product endpoint in rtk query

--- src/store/productsApi.js
+++ src/store/productsApi.js
@@ -3,11 +3,15 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const productsApi = createApi({
   reducerPath: 'productsApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8080' }),
+
   endpoints: builder => ({
     getAllProducts: builder.query({
       query: () => 'products',
     }),
+    getProductById: builder.query({
+      query: slug => `products/${slug}`,
+    }),
   }),
 });
 
-export const { useGetAllProductsQuery } = productsApi;
+export const { useGetAllProductsQuery, useGetProductByIdQuery } = productsApi;
