02a28cb141074018ab79b87442e5fd6f351bd9b8
oğulcan erişti <ogulcaneristi123@gmail.com>
2022-07-05 02:21:14 +0300

    add framer-motion and swiper

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import 'swiper/css/bundle';
 
 ReactDOM.render(
   <Provider store={store}>
