e8377d63f7d8559e3004a565de359167e98f942d
oğulcan erişti <ogulcaneristi123@gmail.com>
2022-07-05 02:25:56 +0300

    add favorites page basis

--- src/pages/Favorites/index.js
+++ src/pages/Favorites/index.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import AnimatedPage from 'components/AnimatedPage';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 const Favorites = () => {
-  return <div>Favorites</div>;
+  const state = useSelector(state => state.favorites);
+  const [favoritesData, setFavoritesData] = useState([]);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setFavoritesData(state.favorites);
+  }, [favoritesData, state, dispatch]);
+
+  return (
+    <AnimatedPage>
+      {favoritesData?.map(item => (
+        <div key={item.id}>
+          <h2>{item.name}</h2>
+          {item.color && <p>Color: {item.color.name}</p>}
+        </div>
+      ))}
+    </AnimatedPage>
+  );
 };
 
 export default Favorites;
