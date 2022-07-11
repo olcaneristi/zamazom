64e3f95030f0a9307461af6665bda16433f6f045
oğulcan erişti <ogulcaneristi123@gmail.com>
2022-07-11 20:01:54 +0300

    add review chart basis
    stylesheet will be replaced to scss

---
+++ src/components/ReviewChart.js
@@ -0,0 +1,59 @@
+import IconStar from 'assets/icons/IconStar';
+import React from 'react';
+
+const ReviewChart = ({ ratings }) => {
+  const containerStyles = {
+    height: 20,
+    width: '100%',
+    backgroundColor: '#f0f0ea',
+    borderRadius: 8,
+    marginBlock: 10,
+  };
+
+  const fillerStyles = {
+    height: '100%',
+    width: `${ratings.value / 1.75}%`,
+    backgroundColor: '#FFD055',
+    borderRadius: 'inherit',
+    textAlign: 'right',
+  };
+
+  const labelStyles = {
+    padding: 5,
+    color: 'white',
+    fontWeight: 'bold',
+  };
+
+  return (
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}>
+      <div
+        style={{
+          marginRight: 5,
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        <IconStar width="14" height="14" />
+        <p style={{ textAlign: 'center', marginLeft: 1, minWidth: 15 }}>{ratings.name}</p>
+      </div>
+      <div style={containerStyles}>
+        <div style={fillerStyles}>
+          <span style={labelStyles}></span>
+        </div>
+      </div>
+      <div
+        style={{
+          marginLeft: 5,
+          display: 'flex',
+          minWidth: 20,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <p style={{ textAlign: 'center' }}>{ratings.value}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ReviewChart;
