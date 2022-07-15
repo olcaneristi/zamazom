74b39b6b5edd198e62e3bfa9a7d09e0eff5cca5e
oğulcan erişti <ogulcaneristi123@gmail.com>
2022-07-15 21:16:01 +0300

    add jwt tokens for user auth

---
+++ server/utils/token.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+const generateToken = user => {
+  const jwtKey = process.env.JWT_KEY;
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+      name: user.name,
+      email: user.email,
+    },
+    jwtKey,
+  );
+
+  return token;
+};
+
+module.exports = generateToken;
