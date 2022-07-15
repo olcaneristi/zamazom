b89146e0ef179ef2740681b799a153a3fb84a036
oğulcan erişti <ogulcaneristi123@gmail.com>
2022-07-15 21:57:34 +0300

    improve register routes

--- server/routes/register.js
+++ server/routes/register.js
@@ -25,11 +25,9 @@ router.post('/', async (req, res) => {
     return res.status(400).send('User already registered');
   }
 
-  user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  });
+  user = new User(req.body);
+
+  console.log(user);
 
   const salt = await bcrypt.genSalt(10);
   user.password = await bcrypt.hash(user.password, salt);
