52638002fd76ba263868cb19ab79949631de3d60
oğulcan erişti <ogulcaneristi123@gmail.com>
2022-07-20 03:42:27 +0300

    add pagination (item per page) to backend

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -2,8 +2,13 @@ const Products = require('../models/products');
 
 const getProducts = async (req, res) => {
   try {
-    const product = await Products.find();
-    res.status(200).json(product);
+    const PAGE_SIZE = 8;
+    const page = parseInt(req.query.page || '0');
+    const total = await Products.countDocuments();
+    const products = await Products.find()
+      .limit(PAGE_SIZE)
+      .skip(page * PAGE_SIZE);
+    res.status(200).json({ totalPages: Math.ceil(total / PAGE_SIZE), products });
   } catch (error) {
     res.status(404).json({
       message: error.message,
