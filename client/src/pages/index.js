44c04bd0c96e4fb8b1a0c65be6ecb773d4216b8a
oğulcan erişti <ogulcaneristi123@gmail.com>
2022-07-21 21:54:04 +0300

    add profile to import/export file

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -5,5 +5,6 @@ import NotFound from './NotFound';
 import Favorites from './Favorites';
 import Login from './Login';
 import Register from './Register';
+import Profile from './Profile';
 
-export { Home, Cart, ItemDetails, NotFound, Favorites, Login, Register };
+export { Home, Cart, ItemDetails, NotFound, Favorites, Login, Register, Profile };
