e8cc1c02eb5d0d1f28beacb8a4920f1828c472ab
oğulcan erişti <ogulcaneristi123@gmail.com>
2022-07-22 13:31:00 +0300

    fix toaster infinite render problem

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 import 'styles/main.scss';
 import MainRouter from 'routes/routes';
+import { Toaster } from 'react-hot-toast';
+import { Fragment } from 'react';
 
 function App() {
-  return <MainRouter />;
+  return (
+    <Fragment>
+      <Toaster containerStyle={{ top: 70 }} />
+      <MainRouter />
+    </Fragment>
+  );
 }
 
 export default App;
