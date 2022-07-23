6692c5b20a31ba7ece089e841d88f50a34a56fba
oğulcan erişti <ogulcaneristi123@gmail.com>
2022-07-23 16:02:21 +0300

    add profile page

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -1,13 +1,18 @@
-import { AnimatedPage, Button, Loader } from 'components';
+import { AnimatedPage, Button, Input, Loader } from 'components';
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import { logoutUser } from 'services/slices/authSlice';
+import moment from 'moment';
 
 const Profile = () => {
   const dispatch = useDispatch();
   const user = useSelector(state => state.auth);
   const [loading, setLoading] = useState(false);
+
+  const convertRelativeTime = date => {
+    return `${moment(date).format('LL ') + `(${moment(date).fromNow()})`}`;
+  };
 
   const handleLogout = () => {
     setLoading(true);
@@ -23,10 +28,29 @@ const Profile = () => {
   }
 
   return (
-    <AnimatedPage style={{ minHeight: '90vh' }}>
-      <h3>Welcome {user.name}</h3>
-      <h3>Your email is {user.email}</h3>
-      <Button onClick={handleLogout}>Logout</Button>
+    <AnimatedPage className="profile">
+      <div className="container profile__container">
+        <div className="profile__header">
+          <h2 className="profile__title">Your Profile</h2>
+        </div>
+
+        <div className="profile__content">
+          <div className="profile__avatar">
+            <img src="https://i.pravatar.cc/120?img" alt="avatar" />
+          </div>
+
+          <div className="profile__info">
+            <Input readOnly value={user.name} placeholder="Name" />
+            <Input readOnly value={user.email} placeholder="Email" />
+            <Input
+              readOnly
+              value={convertRelativeTime(user?.createdAt)}
+              placeholder="Account Create Date"
+            />
+          </div>
+        </div>
+        <Button onClick={handleLogout}>Logout</Button>
+      </div>
     </AnimatedPage>
   );
 };
