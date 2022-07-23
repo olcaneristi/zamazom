e0578b78c881489c5435cfb7362c0df5ccb4c753
oğulcan erişti <ogulcaneristi123@gmail.com>
2022-07-23 16:00:57 +0300

    improve input component

--- client/src/components/Input/index.js
+++ client/src/components/Input/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 
-const Input = ({ type, name, onChange, htmlFor, placeholder, notRequired, value }) => {
+const Input = ({ type, name, onChange, htmlFor, placeholder, notRequired, value, readOnly }) => {
   return (
     <div className="field">
       <input
-        type={type}
+        type={type || 'text'}
         className="field__input"
         name={name}
         value={value}
         onChange={onChange}
         required={notRequired || true}
+        readOnly={readOnly || false}
       />
       <label htmlFor={htmlFor} className="field__label">
         {placeholder}
